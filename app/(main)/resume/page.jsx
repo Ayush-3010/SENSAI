e3e6a27cf2d3c21f6d35adc0a78f612cbe7b3635
Ayush Singhal <ayushsinghal304@gmail.com>
2026-01-14 22:26:27 +0530

    resume(download resume left) and cover letter completed

--- app/(main)/resume/page.jsx
+++ app/(main)/resume/page.jsx
@@ -1,8 +1,15 @@
+import { getResume } from '@/actions/resume'
 import React from 'react'
+import ResumeBuilder from './_components/resume-builder'
 
-const ResumePage = () => {
+const ResumePage = async () => {
+
+  const resume = await getResume()
+
   return (
-    <div>ResumePage</div>
+    <div className='container nx-auto py-6'>
+      <ResumeBuilder initialContent={resume?.content} />
+    </div>
   )
 }
 
